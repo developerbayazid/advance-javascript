c03136ba1a0c9e0511919bc44c019b63e4e67283
developerbayazid <bayazid.freelancer@gmail.com>
2021-11-29 08:00:39 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,22 @@
+//undefined
+let name;
+console.log(name);
+
+let n = "";
+console.log(n);
+
+let x = undefined;
+console.log(x);
+
+const ages = [23, 33, 34, 53];
+console.log(ages[12]);
+
+const eData = {name: 'Hasan', age: 33, job: 'Digital Marketer'}
+console.log(eData.address);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+const result = add(33, 33);
+console.log(result);
